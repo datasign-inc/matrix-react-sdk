63be8d75ed9a58ef5db3e13187ed23ffd7c95057
katsuyoshi ozaki <ozaki@datasign.jp>
2023-11-24 18:39:16 +0900

    Add QR code component for cross-device flow in SIOPv2

---
+++ src/components/views/auth/QRCodeGenerator.tsx
@@ -0,0 +1,114 @@
+/*
+Copyright 2015-2021 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, { Component, ReactNode } from 'react';
+import QRCode from 'qrcode.react';
+
+import {_t} from "../../../languageHandler";
+
+interface IProps {
+    renderingData: string;
+    pollingUri: string;
+    completionForSiopv2(loginToken: string): Promise<void>;
+}
+
+interface IState {
+    showQRCode: boolean;
+}
+
+export default class QRCodeGenerator extends Component<IProps, IState> {
+    private pollingIntervalId: number | null = null;
+    private doPolling = false;
+
+    public constructor(props: IProps) {
+        super(props);
+        this.state = {
+            showQRCode: false,
+        };
+    }
+
+    public componentDidMount(): void {
+        this.startPolling();
+    }
+
+    public componentWillUnmount(): void {
+        this.stopPolling
+    }
+
+    private stopPolling(): void  {
+        if (this.pollingIntervalId) {
+            clearInterval(this.pollingIntervalId);
+        }
+    }
+
+    private startPolling = (): void => {
+        const POLLING_INTERVAL = 1500;
+
+        this.pollingIntervalId = window.setInterval(async () => {
+            try {
+                if (!this.doPolling) {
+                    return
+                }
+                const response = await fetch(this.props.pollingUri);
+
+                if (response.ok) {
+                    const data = await response.json();
+                    if (data && data?.login_token) {
+                        this.stopPolling()
+                        this.props.completionForSiopv2(data.login_token)
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }, POLLING_INTERVAL);
+    };
+
+    private handleButtonClick = (): void => {
+        this.setState({ showQRCode: true });
+    };
+
+    private renderQRCode(): ReactNode {
+        if (this.state.showQRCode) {
+            this.doPolling = true;
+            return (
+                <QRCode value={this.props.renderingData} />
+            );
+        }
+        return null;
+    }
+
+    private renderButton(): ReactNode {
+        if (!this.state.showQRCode){
+            return (
+                <button
+                    className="mx_Login_submit"
+                    onClick={this.handleButtonClick}>{_t("action|wallet_register")}
+                </button>
+            );
+        }
+        return null;
+    }
+
+    public render(): React.ReactNode {
+        return (
+            <div>
+                {this.renderQRCode()}
+                {this.renderButton()}
+            </div>
+        );
+    }
+}
