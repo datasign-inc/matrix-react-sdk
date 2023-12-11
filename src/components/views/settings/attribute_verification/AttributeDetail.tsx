ee7ae42879ee8cb23d1e17c99533b3a2480ba148
katsuyoshi ozaki <ozaki@datasign.jp>
2023-12-11 13:54:51 +0900

    Modifying the appearance and calling new components

--- src/components/views/settings/attribute_verification/AttributeDetail.tsx
+++ src/components/views/settings/attribute_verification/AttributeDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import {VPType} from "./Attribute";
 import {Claims, IssuerInfoClaims} from "./VerifiedAttributes";
+import Modal from "../../../../Modal";
+import IssuerDetail from "./IssuerDetail";
 
 interface IProps {
     vp_type: VPType;
@@ -19,9 +21,17 @@ export default class AttributeDetail extends React.Component<IProps, IState> {
         super(props);
     }
 
+    private issuerDetail = () => {
+        Modal.createDialog(
+            IssuerDetail,
+            {issuer_info: this.props.issuer_info}
+        )
+    }
+
     public render(): React.ReactNode {
         const style = {
             display: 'flex',
+            backgroundColor: "#E9F7F2"
         };
         return (
             <div>
@@ -30,14 +40,21 @@ export default class AttributeDetail extends React.Component<IProps, IState> {
                         <h1>以下の事業者によって認証されました</h1>
                     </span>
                     <div>
-                        <h1>{this.props.issuer_info.issuer_name}</h1>
+                        <h1 style={{marginBottom: "0px"}}>{this.props.issuer_info.issuer_name}</h1>
+                        {this.props.issuer_info.issuer_authenticator_org_name &&
+                            (
+                                <div style={{marginTop: "0px"}}>
+                                    <a onClick={this.issuerDetail}>Verified by {this.props.issuer_info.issuer_authenticator_org_name}</a>
+                                </div>
+                            )
+                        }
                     </div>
                     <div>
-                        <h4>所在地</h4>
+                        <h4 style={{marginTop: "6%", marginBottom: "3px"}}>所在地</h4>
                         {this.props.issuer_info.issuer_address}
                     </div>
                     <div>
-                        <h4>ドメイン名</h4>
+                        <h4 style={{marginBottom: "3px"}} >ドメイン名</h4>
                         {this.props.issuer_info.issuer_domain}
                     </div>
                 </div>
